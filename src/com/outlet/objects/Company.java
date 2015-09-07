1b84893e03c5f303e981a43c6c9da2384db3ce60
kasramp <kasra_mp@yahoo.com>
2015-09-06 19:13:17 -0500

    adding GNU GPL license

--- src/com/outlet/objects/Company.java
+++ src/com/outlet/objects/Company.java
@@ -1,3 +1,20 @@
+/*
+* This file is part of BillMining.
+* 
+* BillMining is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License version 3
+* as published by the Free Software Foundation.
+*
+* BillMining is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.  <http://www.gnu.org/licenses/>
+*
+* Author(s):
+*
+* © 2015 Kasra Madadipouya <kasra@madadipouya.com>
+*/
+
 package com.outlet.objects;
 
 import java.sql.Timestamp;
